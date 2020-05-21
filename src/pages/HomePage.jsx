88fa16cfcd8807af6f1998b92a90550537287db4
Akanimorex <akanimoime2016@gmail.com>
2020-05-21 18:57:07 +0100

    done

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -33,8 +33,8 @@ class HomePage extends React.Component {
   render() {
     return (
       <div>
-        <MDBJumbotron fluid >
-          <MDBContainer className="text-white text-center mt-5" style={{height:'70vh'}}>
+        <MDBJumbotron fluid>
+          <MDBContainer className="text-white text-center mt-5" style={{ height: '70vh' }}>
             <h1 className="h1-responsive display-2">Welcome to FarmSpired</h1>
             <hr />
 
